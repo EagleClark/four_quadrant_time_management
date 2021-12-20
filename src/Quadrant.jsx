a9898def76cbc77c960d5c003843dc6cdf4f2f3d
Eagle Ye <eagleclark@163.com>
2021-12-20 22:38:29 +0800

    完成待完成数据更新

--- src/Quadrant.jsx
+++ src/Quadrant.jsx
@@ -4,6 +4,7 @@ import { IMPORTANT_COLOR, NORMAL_COLOR, URGENT_COLOR, URGENT_IMPORTANT_COLOR } f
 
 export default function Quadrant(props) {
   const { quadrant, data } = props;
+  const todoData = data.filter(v => v.done === 0);
 
   const style = {
     backgroundColor: getColor(quadrant),
@@ -11,7 +12,7 @@ export default function Quadrant(props) {
 
   return (
     <div className='outer' style={style}>
-      {data.length > 0 && <div className='todo'>待完成: {data.length}</div>}
+      {data.length > 0 && <div className='todo'>待完成: {todoData.length}</div>}
       <div className='inner'>
         {data.map(v => <Item key={v.key} itemInfo={v} />)}
       </div>
